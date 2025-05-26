e418804079b8b1df87f0d3496b2c3ae95939701b
Traybi <traybi10@gmail.com>
2025-05-26 17:17:53 +0200

    Separar elementos Archivos

---
+++ WebSegundoTrimestre/productoswebJS.js
@@ -0,0 +1,8 @@
+        window.onscroll = function() {
+        var btn = document.getElementById("btn-top");
+        if (window.pageYOffset > 300) {
+            btn.style.display = "block";
+        } else {
+            btn.style.display = "none";
+        }
+        };
